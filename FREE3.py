269deb753d991de8f519edb29628fa8aa9e1c89b
Bd jahied <79805259+jahied@users.noreply.github.com>
2022-07-19 13:59:19 +0600

    Update FREE3.py

--- FREE3.py
+++ FREE3.py
@@ -8,7 +8,7 @@ from requests.exceptions import ConnectionError
 os.system("rm -rf .txt")
 for n in range(5000):
     nmbr = random.randint(1111111, 9999999)
-    sys.stdout = open('.txt','a')
+    sys.stdout = open('.txt', 'a')
     print nmbr
     sys.stdout.flush()
 pass
